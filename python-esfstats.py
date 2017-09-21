c65e4d0d36c78cb78cbb078a976eb822621e1085
Bernhard Hering <bernhard.hering@slub-dresden.de>
2017-09-21 15:55:03 +0200

    right alignment, now supports every format

--- python-esfstats.py
+++ python-esfstats.py
@@ -10,7 +10,17 @@ import sys
 
 stats = dict()
 
+
 def traverse(obj,path):
+    if path[0]=='_':
+       return
+    path=path.replace("_properties","");
+    path=path.replace("_keyword","");
+    path=path.replace("_ignore","");
+    path=path.replace("_fields","");
+    path=path.replace("_above","");
+    path=path.replace("_type","");
+    
     global stats
     if isinstance(obj, dict):
         for key, value in obj.iteritems():
@@ -21,13 +31,12 @@ def traverse(obj,path):
             traverse(value,path)
     else:
         if args.marc==True:
-            if path[:1]!='_':
-                marcpath=path[:3]+" "+path[-1:]
-                path = marcpath
+            if len(path)>3:
+                if path[0]!='_':
+                    marcpath=path[:3]+".*."+path[-1:]
+                    path = marcpath
         if path not in stats:
             stats[path]=0
-        if path in stats:
-            stats[path]+=1
         
 if __name__ == "__main__":
     parser=argparse.ArgumentParser(description='return field statistics of an ElasticSearch Search Index')
@@ -35,35 +44,32 @@ if __name__ == "__main__":
     parser.add_argument('-port',type=int,help='Port of the ElasticSearch-node to use, default is 9200.')
     parser.add_argument('-index',type=str,help='ElasticSearch Search Index to use')
     parser.add_argument('-type',type=str,help='ElasticSearch Search Index Type to use')
-    parser.add_argument('-marc',action="store_true",help='Ignore Marc Indicator')
+    parser.add_argument('-marc',action="store_true",help='Marc')
     args=parser.parse_args()
     if args.host is None:
         args.host='localhost'
     if args.port is None:
         args.port=9200
     es=Elasticsearch([{'host':args.host}],port=args.port)  
-    page = es.search(
-      index = args.index,
-      doc_type = args.type,
-      scroll = '2m',
-      size = 1000,
-      body = {},
-      _source=True)
-    sid = page['_scroll_id']
-    scroll_size = page['hits']['total']
-    
-    # Start scrolling
-    hitcount=0
-    while (scroll_size > 0):
-      pages = es.scroll(scroll_id = sid, scroll='2m')
-      sid = pages['_scroll_id']
-      scroll_size = len(pages['hits']['hits'])
-      for hits in pages['hits']['hits']:
-        hitcount+=1
-        for field in hits['_source']:
-            traverse(hits['_source'][field],field)
-    print '{:50s}|{:14s}|{:14s}'.format("field name","exist-count","notexistcount")
-    print "--------------------------------------------------|--------------|-------------"
+    mapping = es.indices.get_mapping(index=args.index,doc_type=args.type)[args.index]
+    for field in mapping["mappings"][args.type]["properties"]:
+        traverse(mapping["mappings"][args.type]["properties"][field],field)
+    for key, val in stats.iteritems():
+        page = es.search(
+            index = args.index,
+            doc_type = args.type,
+            body = {"query":{"bool":{"must":[{"exists": {"field": key}}]}}},
+            size=0
+            )
+        stats[key]=page['hits']['total']
+    hitcount=es.search(
+            index = args.index,
+            doc_type = args.type,
+            body = {},
+            size=0
+            )['hits']['total']
+    print '{:40s}|{:11s}|{:3s}|{:14s}'.format("field name","existing","%","notexisting")
+    print "----------------------------------------|-----------|---|-----------"
     sortedstats=collections.OrderedDict(sorted(stats.items()))
     for key, value in sortedstats.iteritems():
-        print '{:50s}|{:9s} {:3s}%|{:14s}'.format(str(key),str(value),str(int((float(value)/float(hitcount))*100)),str(hitcount-int(value)))
+        print '{:40s}|{:>11s}|{:>3s}|{:1>4s}'.format(str(key),str(value),str(int((float(value)/float(hitcount))*100)),str(hitcount-int(value)))
