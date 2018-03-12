7996b5f2ac95f6fde2b96d29c7d01c5ecb56e34b
Thomas Gängler <thomas.gaengler@slub-dresden.de>
2018-03-12 16:07:39 +0100

    added statistic for field cardinality (unique) + field value count (occurrence) + not-existing percentage

--- python-esfstats.py
+++ python-esfstats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import argparse
 import collections
@@ -8,18 +8,20 @@ from elasticsearch import Elasticsearch
 stats = dict()
 
 
-def traverse(dict_or_list, path=[]):
+def traverse(dict_or_list, fieldpath=None):
+    if fieldpath is None:
+        fieldpath = []
     if isinstance(dict_or_list, dict):
         if "properties" in dict_or_list:
             dict_or_list = dict_or_list["properties"]
-        iterator = dict_or_list.iteritems()
+        iterator = dict_or_list.items()
     else:
         iterator = enumerate(dict_or_list)
     for k, v in iterator:
-        yield path + [k], v
+        yield fieldpath + [k], v
         if isinstance(v, (dict, list)):
             if "fields" not in v and "type" not in v:
-                for k, v in traverse(v, path + [k]):
+                for k, v in traverse(v, fieldpath + [k]):
                     yield k, v
 
 
@@ -44,30 +46,67 @@ if __name__ == "__main__":
         for field in path:
             fullpath = fullpath + "." + field
         fullpath = fullpath[1:]
-        if args.marc == True:
+        if args.marc:
             fullpath = fullpath[:3] + ".*." + fullpath[-1:]
-        page = es.search(
+        fieldexistingresponse = es.search(
             index=args.index,
             doc_type=args.type,
             body={"query": {"bool": {"must": [{"exists": {"field": fullpath}}]}}},
             size=0
         )
-        stats[fullpath] = page['hits']['total']
-    hitcount = es.search(
-        index=args.index,
-        doc_type=args.type,
-        body={},
-        size=0
-    )['hits']['total']
-    print('{:11s}|{:3s}|{:11s}|{:40s}'.format("existing", "%", "notexisting", "field name"))
-    print("-----------|---|-----------|----------------------------------------")
+        fullpathkeyword = fullpath + ".keyword"
+        fieldcardinalityresponse = es.search(
+            index=args.index,
+            doc_type=args.type,
+            body={"aggs": {"type_count": {"cardinality": {"field": fullpathkeyword, "precision_threshold": 40000}}}},
+            size=0
+        )
+        fieldvaluecountresponse = es.search(
+            index=args.index,
+            doc_type=args.type,
+            body={"aggs": {"types_count": {"value_count": {"field": fullpathkeyword}}}},
+            size=0
+        )
+        stats[fullpath] = (
+            fieldexistingresponse['hits']['total'],
+            fieldcardinalityresponse['aggregations']['type_count']['value'],
+            fieldvaluecountresponse['aggregations']['types_count']['value']
+        )
+        hitcount = es.search(
+            index=args.index,
+            doc_type=args.type,
+            body={},
+            size=0
+        )['hits']['total']
+
+    print(
+        '{:11s}|{:6s}|{:11s}|{:3s}|{:11s}|{:11s}|{:40s}'.format("existing", "%", "notexisting", "!%", "occurrence",
+                                                                "unique", "field name"))
+    print("-----------|------|-----------|------|-----------|-----------|----------------------------------------")
     sortedstats = collections.OrderedDict(sorted(stats.items()))
-    for key, value in sortedstats.iteritems():
+
+    for key, value in sortedstats.items():
+        fieldexistingcount = value[0]
+        fieldcardinality = value[1]
+        fieldvaluecount = value[2]
+
         keyreplaced = key.replace(u'\ufeff', '')
         keyencoded = keyreplaced.encode('utf-8')
-        valuereplaced = str(value).replace(u'\ufeff', '')
-        valueencoded = valuereplaced.encode('utf-8')
-        print('{:>11s}|{:>3s}|{:>11s}| {:40s}'.format(valueencoded.decode('utf-8'),
-                                                      str(int((float(value) / float(hitcount)) * 100)),
-                                                      str(hitcount - int(value)),
-                                                      '"' + keyencoded.decode('utf-8').replace(".", " > ") + '"'))
+        fieldexistingcountreplaced = str(fieldexistingcount).replace(u'\ufeff', '')
+        fieldexistingcountencoded = fieldexistingcountreplaced.encode('utf-8')
+
+        existing = fieldexistingcountencoded.decode('utf-8')
+        existingpercentage = (float(fieldexistingcount) / float(hitcount)) * 100
+        notexisting = str(hitcount - int(fieldexistingcount))
+        notexistingpercentage = (float(notexisting) / float(hitcount)) * 100
+        occurrence = str(fieldvaluecount)
+        unique = str(fieldcardinality)
+        fieldname = keyencoded.decode('utf-8').replace(".", " > ")
+
+        print('{:>11s}|{:>3.2f}|{:>11s}|{:>3.2f}|{:>11s}|{:>11s}| {:40s}'.format(existing,
+                                                                                 existingpercentage,
+                                                                                 notexisting,
+                                                                                 notexistingpercentage,
+                                                                                 occurrence,
+                                                                                 unique,
+                                                                                 '"' + fieldname + '"'))
